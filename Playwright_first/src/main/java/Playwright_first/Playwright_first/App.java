d1a237784d124dee2bc23d16a332f4b3cc75bbaa
Pradeep <wagh.pradeep2923@gmail.com>
2025-11-02 13:19:28 +0530

    Its first commit

---
+++ Playwright_first/src/main/java/Playwright_first/Playwright_first/App.java
@@ -0,0 +1,10 @@
+package Playwright_first.Playwright_first;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
